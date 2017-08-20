14947113bd351a36e9ec270aa0be91e6c984017a
Nikhil PC <nikhil.masne123@gmail.com>
2017-08-20 13:45:20 +0530

    Initial Commit.

---
+++ src/mypackage1/Starter.java
@@ -0,0 +1,17 @@
+package mypackage1;
+
+public class Starter {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hey Friend+" +mypackage2.Constants.name);
+
+	}
+	
+	
+	public static void sayHNY()
+	{
+		System.out.println(mypackage2.Constants.newYear);
+	}
+
+}
---
+++ src/mypackage2/Constants.java
@@ -0,0 +1,10 @@
+package mypackage2;
+
+public class Constants {
+	public static String name = "Simple Name";
+
+	public static String welcome = "Welcom Friend... ";
+
+	public static String newYear = "Happy New Year!!!";
+
+}
