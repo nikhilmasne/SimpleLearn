5fd48de629213123ed85a9da56af69e522c2b9bb
nmasne <nikhilmasne007@gmail.com>
2017-08-20 15:03:13 +0530

    Added package3

---
+++ src/mypackage3/Functions.java
@@ -0,0 +1,15 @@
+package mypackage3;
+
+public class Functions {
+	
+	public static void sayHNY()
+	{
+		System.out.println(mypackage2.Constants.newYear);
+	}
+	
+	public static void sayWelcome()
+	{
+		System.out.println(mypackage2.Constants.welcome);
+	}
+
+}
